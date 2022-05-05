c9ca4f92d289081017cf49f10de3eeb0dbb957d5
Ryan <ryan.shieh2020@gmail.com>
2022-05-05 16:29:19 -0700

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Ryan Shieh on 5/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
